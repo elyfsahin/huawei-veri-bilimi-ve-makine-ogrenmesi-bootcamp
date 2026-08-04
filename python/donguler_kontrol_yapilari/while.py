a6e07fada97832c9c9f0cfb9fd26f5b754124ac8
elyfsahin <eelifsahinn00@gmail.com>
2026-08-04 13:54:30 +0300

    add while loop examples

---
+++ python/donguler_kontrol_yapilari/while.py
@@ -0,0 +1,20 @@
+i=0
+while i<6:
+    print(i)
+    i+=1
+
+sayac=1
+while sayac<=4:
+    print("Merhaba")
+    sayac+=1
+
+i=0
+while i < 12:
+    if i%2==0:
+        print(f"Çift: {i}")
+    i+=1
+
+giris=""
+while giris!="q":
+    giris = input("Çıkmak için q yazınız: ")
+    print(f"Kullanıcı mesajı: {giris}")
