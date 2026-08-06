1df9643469150a4907f6eab5b762a183607324ea
elyfsahin <eelifsahinn00@gmail.com>
2026-08-07 00:01:23 +0300

    add scope examples

---
+++ python/fonksiyonlar/scope.py
@@ -0,0 +1,40 @@
+"""
+Scope (local ve global)
+    - bir değişkenin nerede erişilebilir olduğunu ifade eder
+    - bir değişken nerede tanımlıysa orada geçerlidir.
+"""
+
+#local değişken: fonksiyon içerisinde tanımlanan değişken
+
+def test():
+    x=10
+    print(f"Fonksiyon içi {x}")
+
+test()
+
+#global değişken: fonksiyon dışında tanımlanan değişken
+x=15
+
+def test():
+    print(f"Fonksiyon içi {x}")
+
+test()
+
+#aynı isimli değişkenler
+x=11
+def test():
+    x=5
+    print(f"Fonksiyon içi {x}")
+
+test()
+print(f"Fonksiyon dışı {x}")
+
+#global anahtar kelimesi
+
+x=9
+def test():
+    global x
+    x=5 #lokal -> global
+
+test()
+print(x)
