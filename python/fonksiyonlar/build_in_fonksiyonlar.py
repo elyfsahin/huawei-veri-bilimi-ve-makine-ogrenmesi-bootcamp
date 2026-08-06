e755d450b6591de2a600f31f3564d7e582d03048
elyfsahin <eelifsahinn00@gmail.com>
2026-08-06 23:41:05 +0300

    add build in functions examples

---
+++ python/fonksiyonlar/build_in_fonksiyonlar.py
@@ -0,0 +1,31 @@
+# print: ekrana çıktı üretir
+print("merhaba")
+
+# len: bir veri yapısının uzunluğunu verir
+liste = [1, 2, 3]
+print(len(liste))
+
+# type: bir değişkenin veri tipini verir
+x = 3.14
+print(type(x)) # <class 'float'>
+
+#sum, max, min
+sayilar=[1,2,3,4,5,6]
+print(sum(sayilar))
+print(max(sayilar))
+print(min(sayilar))
+
+#abs
+x=-8
+print(abs(x))
+
+#round
+x=2.4578765467
+print(round(x,3))
+
+#sorted
+sayilar=[13,8,1,4,6,7,12,9]
+print(sorted(sayilar)) # küçükten büyüğe sıralar
+
+
+
